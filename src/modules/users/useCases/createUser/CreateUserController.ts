bd5f5ee1b095bfb034b23e9aaea00d7774fa652f
Pedro Tassinari <pedro.tassinari99@gmail.com>
2021-06-09 21:42:24 -0300

    challenge done

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,11 +3,17 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        try {
+            const user = this.createUserUseCase.execute(request.body);
+
+            return response.status(201).send(user);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    }
 }
 
 export { CreateUserController };
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,18 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.headers as { user_id: string };
+
+        try {
+            const all = this.listAllUsersUseCase.execute({ user_id });
+            return response.json(all);
+        } catch (err) {
+            return response.status(400).json({ error: err.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
